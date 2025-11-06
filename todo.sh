94196eaf0620f7fb3b3ac77f4e7bb2a29a40af6c
GufNZ <j.chris.findlay@gmail.com>
2025-11-06 16:56:09 +1300

    forgot a fix, but also new issues

--- todo.sh
+++ todo.sh
@@ -1,1 +1,2 @@
 //BUG: proc type is often not matching, and needs to understand #dump etc
+//FIXME: stdLib names used in declarations drop the declaration styles.  //QUESTION: should they include stdLib at all?
